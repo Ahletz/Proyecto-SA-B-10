1c95f7f2bc66252f6d20ef0d7c2481164ff040aa
aidaorantes9 <aidaorantes9@gmail.com>
2026-09-24 12:37:16 -0600

    feat(infra): agregar rangos secundarios (pods/services) a la subred para GKE

--- infrastructure/terraform/network.tf
+++ infrastructure/terraform/network.tf
@@ -8,6 +8,16 @@ resource "google_compute_subnetwork" "subnet" {
   ip_cidr_range = "10.10.0.0/24"
   region        = var.region
   network       = google_compute_network.vpc.id
+
+  secondary_ip_range {
+    range_name    = "gke-pods"
+    ip_cidr_range = "10.20.0.0/16"
+  }
+
+  secondary_ip_range {
+    range_name    = "gke-services"
+    ip_cidr_range = "10.30.0.0/20"
+  }
 }
 
 # Rango reservado para que Cloud SQL (servicio administrado) se conecte
